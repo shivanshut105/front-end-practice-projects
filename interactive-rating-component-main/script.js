e79a0e26da9252511695984b74e393b319fa285f
Shivanshu <shivanshut105@gmail.com>
2023-06-22 00:15:13 +0530

    added interactive-rating-component

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,1 +1,20 @@
-document.getElementsByClassName("btn-secondary")
+const mainContainer = document.querySelector(".container");
+const thanksContainer = document.querySelector(".thank-you-state");
+
+const submitButton = document.getElementById("submit");
+
+const rating = document.getElementById("rating-scale");
+const ratingButtons = document.querySelectorAll(".btn-secondary");
+
+submitButton.addEventListener("click", ()=>{
+    if(rating.innerHTML=="0") return;
+    thanksContainer.classList.remove("hidden");
+    mainContainer.style.display = "none";
+})
+
+ratingButtons.forEach(rate => {
+    rate.addEventListener("click", ()=>{
+        rating.innerHTML = rate.innerHTML;
+        console.log(rate.innerHTML);
+    })
+});
